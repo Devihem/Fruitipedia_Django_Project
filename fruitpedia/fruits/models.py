cc96be43af1523a85de4bf000bc10f9fdcf7f734
Devihem <Devihem7@gmail.com>
2023-12-08 17:56:46 +0200

    Added properly working Category in  Fruits Description

--- fruitpedia/fruits/models.py
+++ fruitpedia/fruits/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import *
 from fruitpedia.fruits.validators import only_alphabets
 
+
 # Create your models here.
 
 class Category(models.Model):
@@ -15,11 +16,15 @@ class Category(models.Model):
 
 
 class Fruit(models.Model):
+    CHOICES_CATEGORY = [(str(category.name), str(category.name)) for category in Category.objects.all()]
+
     name = models.CharField(
         max_length=30,
         validators=[MinLengthValidator(2), MaxLengthValidator(30), only_alphabets])
 
     image_url = models.URLField()
+
+    category = models.CharField(max_length=255, choices=CHOICES_CATEGORY)
 
     description = models.TextField()
 
